756cf1878c612d794deef7f5628db613b85e3f14
usunyu <usunyu@gmail.com>
2014-06-02 21:10:56 -0700

    Programming Question-3 initial

---
+++ Algorithms Design and Analysis, Part 1/Programming Question-3/RandomizedContraction.java
@@ -0,0 +1,20 @@
+/*
+Your task is to code up and run the randomized contraction algorithm for the min cut problem and use it on the above
+graph to compute the min cut. (HINT: Note that you'll have to figure out an implementation of edge contractions. 
+Initially, you might want to do this naively, creating a new graph from the old every time there's an edge 
+contraction. But you should also think about more efficient implementations.) (WARNING: As per the video lectures, 
+please make sure to run the algorithm many times with different random seeds, and remember the smallest cut that you 
+ever find.) Write your numeric answer in the space provided. So e.g., if your answer is 5, just type 5 in the space 
+provided.
+*/
+
+public class RandomizedContraction {
+	public static void main(String[] args) {
+		if(args.length == 0) {
+			System.err.println("Please input file path.");
+			System.exit(-1);
+		}
+
+		
+	}
+}
