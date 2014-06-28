cf4b8b26eab027745d0f75c3ffbf6f7a300e13c8
usunyu <usunyu@gmail.com>
2014-06-28 15:38:18 -0700

    Programming Question-6 use hashset

--- Algorithms Design and Analysis, Part 1/Programming Question-6/TwoSumVariant.java
+++ Algorithms Design and Analysis, Part 1/Programming Question-6/TwoSumVariant.java
@@ -1,4 +1,6 @@
 /*
+Question 1
+
 The goal of this problem is to implement a variant of the 2-SUM algorithm (covered in the Week 6 lecture on hash 
 table applications).
 The file contains 1 million integers, both positive and negative (there might be some repetitions!).This is your 
@@ -14,5 +16,54 @@ Write your numeric answer (an integer between 0 and 20001) in the space provided
 OPTIONAL CHALLENGE: If this problem is too easy for you, try implementing your own hash table for it. For example, 
 you could compare performance under the chaining and open addressing approaches to resolving collisions.
 */
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Scanner;
 
-
+public class TwoSumVariant {
+	private static ArrayList<Long> numbers;
+	private final static int DefaultSize = 1000000;
+	
+	private static void input(String path) {
+		System.out.println("Processing input file...");
+		try {
+			numbers = new ArrayList<Long>(DefaultSize);
+			File file = new File(path);
+			Scanner sc = new Scanner(file);
+			while(sc.hasNextLong())
+				numbers.add(sc.nextLong());
+			sc.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	// using HashSet
+	private static int twoSumVariant() {
+		System.out.println("Counting two sum...");
+		int count = 0;
+		for(int target = -10000; target <= 10000; target++) {
+			HashSet<Long> set = new HashSet<Long>();
+			for(long i : numbers) {
+				long need = target - i;
+				if(set.contains(need)) {
+					count++;
+					break;
+				}
+				set.add(i);
+			}
+		}
+		return count;
+	}
+	
+	public static void main(String[] args) {
+		if (args.length == 0) {
+			System.err.println("Please input file path.");
+			System.exit(-1);
+		}
+		input(args[0]);
+		System.out.println(twoSumVariant());
+	}
+}
