a5d056debcb4d14e8641888db19febfa0435299a
usunyu <usunyu@gmail.com>
2014-06-28 15:56:44 -0700

    Programming Question-6 Q1

--- Algorithms Design and Analysis, Part 1/Programming Question-6/TwoSumVariant.java
+++ Algorithms Design and Analysis, Part 1/Programming Question-6/TwoSumVariant.java
@@ -15,56 +15,85 @@ Write your numeric answer (an integer between 0 and 20001) in the space provided
 
 OPTIONAL CHALLENGE: If this problem is too easy for you, try implementing your own hash table for it. For example, 
 you could compare performance under the chaining and open addressing approaches to resolving collisions.
-*/
+ */
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.Scanner;
 
 public class TwoSumVariant {
 	private static ArrayList<Long> numbers;
 	private final static int DefaultSize = 1000000;
-	
+
 	private static void input(String path) {
 		System.out.println("Processing input file...");
 		try {
 			numbers = new ArrayList<Long>(DefaultSize);
 			File file = new File(path);
 			Scanner sc = new Scanner(file);
-			while(sc.hasNextLong())
+			while (sc.hasNextLong())
 				numbers.add(sc.nextLong());
 			sc.close();
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	// using HashSet
 	private static int twoSumVariant() {
 		System.out.println("Counting two sum...");
 		int count = 0;
-		for(int target = -10000; target <= 10000; target++) {
+		for (long target = -10000; target <= 10000; target++) {
 			HashSet<Long> set = new HashSet<Long>();
-			for(long i : numbers) {
+			for (long i : numbers) {
 				long need = target - i;
-				if(set.contains(need)) {
-					if(need != i)
+				if (set.contains(need)) {
+					if (need != i) {
 						count++;
-					break;
+						break;
+					}
 				}
 				set.add(i);
 			}
+			if (target % 100 == 0)
+				System.out.println((double) (target + 10000) / 20000 + "%");
 		}
 		return count;
 	}
-	
+
+	// useing sorting
+	private static int twoSumVariant2() {
+		System.out.println("Counting two sum...");
+		Collections.sort(numbers);
+		int count = 0;
+		for (long target = -10000; target <= 10000; target++) {
+			int start = 0, end = numbers.size() - 1;
+			while (start < end) {
+				long sum = numbers.get(start) + numbers.get(end);
+				if (sum == target) {
+					if (numbers.get(start) != numbers.get(end))
+						count++;
+					break;
+				} else if (sum < target) {
+					start++;
+				} else {
+					end--;
+				}
+			}
+			if (target % 100 == 0)
+				System.out.println((double) (target + 10000) / 20000 * 100 + "%");
+		}
+		return count;
+	}
+
 	public static void main(String[] args) {
 		if (args.length == 0) {
 			System.err.println("Please input file path.");
 			System.exit(-1);
 		}
 		input(args[0]);
-		System.out.println(twoSumVariant());
+		System.out.println(twoSumVariant2());
 	}
 }
