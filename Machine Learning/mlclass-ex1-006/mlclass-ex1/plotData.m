ad982b4993ab79dce0e35ee9ec6691e2040fb601
usunyu <usunyu@gmail.com>
2014-07-14 17:40:35 -0700

    [Machine Learning] Ex1 computeCost

--- Machine Learning/mlclass-ex1-006/mlclass-ex1/plotData.m
+++ Machine Learning/mlclass-ex1-006/mlclass-ex1/plotData.m
@@ -16,9 +16,9 @@ function plotData(x, y)
 
 figure; % open a new figure window
 
-
-
-
+plot(x, y, 'rx', 'MarkerSize', 10); % Plot the data
+ylabel('Profit in $10,000s'); % Set the y − axis labe
+xlabel('Population of City in 10,000s'); % Set the x − axis label
 
 
 % ============================================================
