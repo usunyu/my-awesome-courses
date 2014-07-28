7595549589b53b8ba74ba74fd07c29943999db9e
usunyu <usunyu@gmail.com>
2014-07-27 22:52:26 -0700

    [Machine Learning] Ex3 oneVsAll

--- Machine Learning/mlclass-ex3-006/mlclass-ex3/oneVsAll.m
+++ Machine Learning/mlclass-ex3-006/mlclass-ex3/oneVsAll.m
@@ -50,14 +50,14 @@ X = [ones(m, 1) X];
 %
 
 
+for c = 1:num_labels
+	initial_theta = zeros(n + 1, 1);
+	options = optimset('GradObj', 'on', 'MaxIter', 500);
 
+	[theta] = fmincg (@(t)(lrCostFunction(t, X, (y == c), lambda)), initial_theta, options);
 
-
-
-
-
-
-
+	all_theta(c,:) = theta';
+end
 
 
 % =========================================================================
