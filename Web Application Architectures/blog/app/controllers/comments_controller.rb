19379a7d320ba304a96cdfe8ec7107c95a302017
Yu Sun <usunyu@gmail.com>
2014-11-30 18:58:15 -0800

    [Web Application Architectures] The Blog App – Iteration 5

--- Web Application Architectures/blog/app/controllers/comments_controller.rb
+++ Web Application Architectures/blog/app/controllers/comments_controller.rb
@@ -32,6 +32,7 @@ class CommentsController < ApplicationController
       if @comment.save
         format.html { redirect_to @post, notice: 'Comment was successfully created.' }
         format.json { render :show, status: :created, location: @comment }
+        format.js {} #create.js.erb
       else
         format.html { render :new }
         format.json { render json: @comment.errors, status: :unprocessable_entity }
